33a52b77fc282376aa5880f7c3a17ed2f230f798
Nay Myo Lwin <naymyolwin@Nays-MacBook-Pro.local>
2023-02-22 22:35:08 +0700

    to redeploy

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const NotFound = () => {
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/alt-react-movie-finder-2">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">
           Movie Finder
